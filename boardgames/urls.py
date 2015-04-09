9ffb43a630802290ce8cd1c10e1691953f2edff0
Jean Guzman <jean.guzman@ceroic.com>
2015-04-09 13:29:05 -0600

    urls.py - added new route and mapped to a new view views.py - created new view to return string to /

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import HelloWorldView
+
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'boardgames.views.home', name='home'),
+    url(r'^$', HelloWorldView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
