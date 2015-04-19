5d6414934913da922280397b2bae6b64c21743b5
Jean Guzman <jean.guzman@ceroic.com>
2015-04-19 13:18:30 -0600

    urls.py - added more urls to patternurls login.html - added new template file

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -1,12 +1,20 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
 from .views import HelloWorldView
 
 urlpatterns = [
     # Examples:
-    url(r'^$', HelloWorldView.as_view(), name='home'),
+    url(r'^$', 'main.views.home', name='boardgames_home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+
+    url(r'^login/$', 'login', {'template_name': 'login.html'}, name='boardgames_login'),
+
+    url(r'^logout/$', 'logout', {'next_page': 'boardgames_home'}, name='boardgames_logout'),
+)
