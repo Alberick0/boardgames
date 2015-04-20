6e8931aaf24a2781ba174d50412e144fffa07b81
Jean Guzman <jean.guzman@ceroic.com>
2015-04-20 10:48:15 -0600

    Modified user home page to display now all the user games info
    *TEMPLATES*
    Added new:
    game_list_snippet.html - outputs the passed game list to the home
    
    Modified:
    user/home.html - includes game_list_snippet with game lists info
    
    *VIEWS*
    Modified:
    user.views - modified home view to gather games info
    
    *MODELS*
    Modified:
    tictactoe.models - added new model Manager with new method and assigned it to Game class

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 
 STATUS_CHOICES = (
@@ -10,6 +11,17 @@ STATUS_CHOICES = (
 )
 
 
+class GamesManager(models.Manager):  # Custom manager class, which represents the whole table
+    def games_for_user(self, user):
+        """
+        Return a queryset of games that this user participates in
+        """
+
+        return super(GamesManager, self).get_queryset().filter(
+            Q(first_player_id=user.id) | Q(second_player_id=user.id)
+        )  # this query returns all games where the user is either 1stP or 2ndP
+
+
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name='games_first_player')
     second_player = models.ForeignKey(User, related_name='games_second_player')
@@ -17,6 +29,7 @@ class Game(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='A', choices=STATUS_CHOICES)
+    objects = GamesManager()  # sets a new manager for this class.
 
     def __str__(self):
         return '{} vs {}'.format(self.first_player, self.second_player)
@@ -27,3 +40,6 @@ class Move(models.Model):
     y = models.IntegerField()
     comment = models.CharField(max_length=150)
     game = models.ForeignKey(Game)
+
+    def __str__(self):
+        return 'Next to move is {}'.format(self.game.next_to_move)
