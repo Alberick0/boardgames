28a4c4a733a257f07b0bce02cae9a51ca49f3410
Jean Guzman <jean.guzman@ceroic.com>
2015-04-28 18:07:09 -0600

    *TEMPLATES* main/home - updated url to signup to use name defined in url file user/signup - new template to allow user registration
    *VIEWS*
    new class using generic CreateView class to handle user registration
    
    *URLS*
    user.urls - plugged new url to use new class view

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
 
+from .views import SingUpView
+
 urlpatterns = patterns(
     'user.views',
-    url(r'^home$', 'home', name='user_home')
+    url(r'^home$', 'home', name='user_home'),
+    url(r'^signup$', SingUpView.as_view(), name='user_signup'),
 )
